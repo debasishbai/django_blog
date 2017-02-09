2486a86e91a6f417799a8072850a1dd29831c281
debasishbai <bai.debasish123@gmail.com>
2017-02-09 20:39:10 +0530

    added login/logout option

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from .models import Post
 from .forms import PostForm
 
 
@@ -14,23 +15,27 @@ def post_detail(request, pk):
     return render(request, "blog/post_detail.html", {"post": post})
 
 
+@login_required()
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by("creation_date")
     return render(request, "blog/post_draft_list.html", {"posts": posts})
 
 
+@login_required()
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect("post_detail", pk=pk)
 
 
+@login_required()
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect("post_list")
 
 
+@login_required()
 def post_new(request):
 
     if request.method == "POST":
@@ -47,6 +52,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required()
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
